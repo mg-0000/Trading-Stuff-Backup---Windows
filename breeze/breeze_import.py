9b52d3b0227d902d05c4163fa38e3c45e0e77233
Mridul <mridulg.2003@gmail.com>
2024-01-20 12:49:42 +0530

    Moving to new branch

--- breeze/breeze_import.py
+++ breeze/breeze_import.py
@@ -2,7 +2,7 @@ from breeze_connect import BreezeConnect
 ### Mridul's credentials
 api_key="650G7Z51z645540%&15~b93v5*4M!574"
 api_secret="409755400@8P#xT7009=x6~O58977333"
-session_token="32113532"
+session_token="32300759"
 breeze = BreezeConnect(api_key=api_key)
 breeze.generate_session(api_secret=api_secret, session_token=str(session_token))
 # print(breeze.get_customer_details(api_session=session_token))
@@ -18,7 +18,7 @@ breeze.generate_session(api_secret=api_secret, session_token=str(session_token))
 
 api_key_2="0w$S7143J@857669k4`H7O2372K668nq"
 api_secret_2="5439143UN65!xzt0#6q_70818~r6892+"
-session_token_2="32113726"
+session_token_2="32300765"
 breeze_2 = BreezeConnect(api_key=api_key)
 breeze_2.generate_session(api_secret=api_secret, session_token=str(session_token))
 
